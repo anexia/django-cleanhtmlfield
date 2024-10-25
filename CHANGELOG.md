ac3b3c8176868d6f670d3b2869e5bdb08bfcf55c
Bernhard Hagmann <mail@bernhard-hagmann.com>
2024-10-25 11:28:29 +0200

    SIANXKE-404: Update Python, Django, and package versions

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,14 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [1.4.0]
+### Removed
+- Dropped support for Django 3.2, 4.0, 4.1
+- Dropped support for Python 3.7 and 3.8
+### Added
+- Added support for Django 4.2, 5.0, and 5.1
+- Added support for Python 3.11, 3.12, and 3.13
+
 ## [1.3.0]
 ### Removed
 - Dropped support for Django 2.2
@@ -37,7 +45,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Added
 - Initial Release
 
-[Unreleased]: https://github.com/anexia/django-cleanhtmlfield/compare/v1.3.0...HEAD
+[Unreleased]: https://github.com/anexia/django-cleanhtmlfield/compare/v1.4.0...HEAD
+[1.4.0]: https://pypi.org/project/django-cleanhtmlfield/1.4.0/
 [1.3.0]: https://pypi.org/project/django-cleanhtmlfield/1.3.0/
 [1.2.0]: https://pypi.org/project/django-cleanhtmlfield/1.2.0/
 [1.1.0]: https://pypi.org/project/django-cleanhtmlfield/1.1.0/
--- tests/cleanhtmlfield_testapp/tests_clean_styles.py
+++ tests/cleanhtmlfield_testapp/tests_clean_styles.py
@@ -9,17 +9,17 @@ class TestCleanStyles(SimpleTestCase):
         Tests that cleaning css styles leaves styles with multiple dots in place
         :return:
         """
-        self.assertEquals(clean_styles("color:red:foo();"), "color:red:foo();")
+        self.assertEqual("color:red:foo();", clean_styles("color:red:foo();"))
 
     def test_clean_styles_adds_semicolon(self):
         """
         Tests whether clean styles add a semicolon
         :return:
         """
-        self.assertEquals(clean_styles("color:red"), "color:red;")
+        self.assertEqual("color:red;", clean_styles("color:red"))
 
-        self.assertEquals(
-            clean_styles("color:red; font-size:10pt"), "color:red;font-size:10pt;"
+        self.assertEqual(
+            "color:red;font-size:10pt;", clean_styles("color:red; font-size:10pt")
         )
 
     def test_clean_styles_single(self):
@@ -27,20 +27,20 @@ class TestCleanStyles(SimpleTestCase):
         Tests that cleaning styles removes extra spaces
         :return:
         """
-        self.assertEquals(clean_styles("padding: 9px; "), "padding:9px;")
+        self.assertEqual("padding:9px;", clean_styles("padding: 9px; "))
 
-        self.assertEquals(clean_styles("margin: 9px; "), "margin:9px;")
+        self.assertEqual("margin:9px;", clean_styles("margin: 9px; "))
 
-        self.assertEquals(clean_styles("padding-top: 9px; "), "padding-top:9px;")
+        self.assertEqual("padding-top:9px;", clean_styles("padding-top: 9px; "))
 
     def test_clean_styles_multiple(self):
         """
         Tests that cleaning styles works with multiple styles
         :return:
         """
-        self.assertEquals(
-            clean_styles("padding: 9px; margin-top: 10px; margin-bottom: 10px;"),
+        self.assertEqual(
             "padding:9px;margin-top:10px;margin-bottom:10px;",
+            clean_styles("padding: 9px; margin-top: 10px; margin-bottom: 10px;"),
         )
 
     def test_clean_styles_invalid_element(self):
@@ -48,11 +48,11 @@ class TestCleanStyles(SimpleTestCase):
         Tests that cleaning styles removes invalid elements
         :return:
         """
-        self.assertEquals(
+        self.assertEqual(
+            "padding:9px;margin-bottom:10px;",
             clean_styles(
                 "padding: 9px; some-invalid-attribute: 10px; margin-bottom: 10px;"
             ),
-            "padding:9px;margin-bottom:10px;",
         )
 
     def test_clean_styles_preserve_styles_whitespace(self):
@@ -61,18 +61,18 @@ class TestCleanStyles(SimpleTestCase):
         :return:
         """
         with self.settings(PRESERVE_STYLES_WHITESPACE=True):
-            self.assertEquals(clean_styles("padding: 9px; "), "padding: 9px;")
+            self.assertEqual("padding: 9px;", clean_styles("padding: 9px; "))
 
-            self.assertEquals(clean_styles("padding : 9px; "), "padding : 9px;")
+            self.assertEqual("padding : 9px;", clean_styles("padding : 9px; "))
 
-            self.assertEquals(clean_styles("padding:  9px; "), "padding:  9px;")
+            self.assertEqual("padding:  9px;", clean_styles("padding:  9px; "))
 
-            self.assertEquals(
-                clean_styles("padding: 9px; margin-top: 10px; margin-bottom: 10px;"),
+            self.assertEqual(
                 "padding: 9px; margin-top: 10px; margin-bottom: 10px;",
+                clean_styles("padding: 9px; margin-top: 10px; margin-bottom: 10px;"),
             )
 
-            self.assertEquals(
-                clean_styles("padding: 9px;margin-top: 10px;margin-bottom: 10px;"),
+            self.assertEqual(
                 "padding: 9px;margin-top: 10px;margin-bottom: 10px;",
+                clean_styles("padding: 9px;margin-top: 10px;margin-bottom: 10px;"),
             )
